ca4f4a24d910ca693c40b0767ba77c25231dd34e
Christopher Plummer <cplummer@reservations.com>
2021-10-09 20:08:02 -0400

    Typescript

--- js/friendlyRepository.js
+++ js/friendlyRepository.js
@@ -12,11 +12,12 @@ export async function getUsers() {
 }
 
 export async function createUser() {
-    let users = await fetch('https://friendly-api.azurewebsites.net/users').then(
+    let users = await ('https://friendly-api.azurewebsites.net/users').then(
         function(response) {
-            if (!response.ok) {
+            if (response.status != 201) {
                 throw new Error(`HTTP error! status: ${response.status}`);
             }
+            console.log(response.status);
             return response.json();
         }
     );
